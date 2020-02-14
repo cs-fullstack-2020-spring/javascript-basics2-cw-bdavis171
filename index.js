a12840a7ce464d3aed758328eb08aecae68238fa
bdavis171 <bdavis171@gmail.com>
2020-02-14 13:49:41 -0600

    Worked Finished

---
+++ index.js
@@ -0,0 +1,18 @@
+//Question 1
+let myName = "My name is: ";
+let name = "Brandon Davis";
+console.log(myName + name);
+
+// Question 2
+let balance = prompt("What is your balance?");
+alert("You lost $10. Your new balance is: $"+(Number(balance)-10));
+
+// Question 3
+let grade = prompt("Enter your number grade");
+let extra = prompt("Enter you extra credit");
+alert("You grade total: " +(Number(grade)+(Number(extra))));
+
+// Question 4
+let balance1 = prompt("What is your balance?");
+let deduction = prompt("How much do you want to deduct?");
+alert("Your remaining balance is: " + (balance1-deduction));
